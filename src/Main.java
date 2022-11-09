550a8ca981e0d57f581f100d2ff26559c3573c93
HannahÖstebo <hannahostebo@gmail.com>
2022-11-10 00:39:54 +0100

    Första versionen!

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hej! Nu har jag fått möjlighet att gå igenom allt igen." +
+                "och ÄNTLIGEN har det fastnat!");
+        NyKlass.nyKlass();
+    }
+}
---
+++ src/NyKlass.java
@@ -0,0 +1,5 @@
+public class NyKlass {
+    public static void nyKlass() {
+        System.out.println("Nu java-klass som får lite kod!");
+    }
+}
